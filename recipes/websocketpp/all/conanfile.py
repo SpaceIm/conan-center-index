515180071bbf2ce1d003350f954abadb9277900b
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-08-17 11:13:59 +0200

    websocketpp: cleanup

--- recipes/websocketpp/all/conanfile.py
+++ recipes/websocketpp/all/conanfile.py
@@ -1,5 +1,5 @@
 import os
-from conans import ConanFile, tools, CMake
+from conans import ConanFile, tools
 
 
 class WebsocketPPConan(ConanFile):
@@ -9,12 +9,10 @@ class WebsocketPPConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/zaphoyd/websocketpp"
     license = "BSD-3-Clause"
-    settings = "os", "arch", "compiler", "build_type"
-    exports_sources = ["CMakeLists.txt", "patches/*"]
-    generators = ["cmake"]
+    exports_sources = "patches/**"
     options = {"asio": ["boost", "standalone"]}
     default_options = {"asio": "boost"}
-    
+
     @property
     def _source_subfolder(self):
         return "source_subfolder"
@@ -23,29 +21,28 @@ class WebsocketPPConan(ConanFile):
         self.requires("openssl/1.1.1g")
         self.requires("zlib/1.2.11")
         if self.options.asio == "standalone":
-            self.requires("asio/1.16.1")
+            self.requires("asio/1.17.0")
         else:
             self.requires("boost/1.73.0")
+
+    def package_id(self):
+        self.info.header_only()
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
-        self._patch_sources()
 
-    def _patch_sources(self):
-        if "patches" in self.conan_data and self.version in self.conan_data["patches"]:
-            for patch in self.conan_data["patches"][self.version]:
-                tools.patch(**patch)
+    def build(self):
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            tools.patch(**patch)
 
     def package(self):
         self.copy(pattern="COPYING", dst="licenses", src=self._source_subfolder)
-        # We have to copy the headers manually, since the upstream cmake.install() step doesn't do so.
-        self.copy(pattern="*.hpp", dst="include/websocketpp", src=self._source_subfolder + "/websocketpp")
+        self.copy(pattern="*.hpp",
+                  dst=os.path.join("include", "websocketpp"),
+                  src=os.path.join(self._source_subfolder, "websocketpp"))
 
     def package_info(self):
         if self.options.asio == "standalone":
             self.cpp_info.defines.extend(["ASIO_STANDALONE", "_WEBSOCKETPP_CPP11_STL_"])
-
-    def package_id(self):
-        self.info.header_only()
