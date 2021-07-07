6e4011f17229270f0d4692ffee8830d1a5a1357b
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-08 01:57:13 +0200

    reorder methods

--- recipes/libsodium/all/conanfile.py
+++ recipes/libsodium/all/conanfile.py
@@ -64,15 +64,15 @@ class LibsodiumConan(ConanFile):
             "16": "vs2019"
         }.get(str(self.settings.compiler.version), False)
 
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
     def configure(self):
         if self.options.shared:
             del self.options.fPIC
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
-
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
 
     def validate(self):
         if self.settings.compiler == "Visual Studio":
