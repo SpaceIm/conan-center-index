9935ca425e3dec2e490f8a50175acdfa08db5f8d
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-08 00:20:46 +0200

    no os.rename

--- recipes/libsodium/all/conanfile.py
+++ recipes/libsodium/all/conanfile.py
@@ -2,6 +2,8 @@ from conans import ConanFile, AutoToolsBuildEnvironment, tools, MSBuild
 from conans.errors import ConanInvalidConfiguration
 import os
 
+required_conan_version = ">=1.33.0"
+
 
 class LibsodiumConan(ConanFile):
     name        = "libsodium"
@@ -79,9 +81,8 @@ class LibsodiumConan(ConanFile):
             self.build_requires("libtool/2.4.6")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
 
     def _build_visual(self):
         sln_path = os.path.join(self.build_folder, self._source_subfolder, "builds", "msvc", self._vs_sln_folder, "libsodium.sln")
