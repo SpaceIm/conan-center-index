3a10deaf1689af9c0d8d0c701a659aa59b26d229
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-07-08 01:48:43 +0200

    refactoring of autotools build
    - call install target in package()
    - move emscripten build/install to dedicated methods

--- recipes/libsodium/all/conanfile.py
+++ recipes/libsodium/all/conanfile.py
@@ -31,6 +31,8 @@ class LibsodiumConan(ConanFile):
         "PIE": False,
     }
 
+    _autotools = None
+
     @property
     def _source_subfolder(self):
         return "source_subfolder"
@@ -90,67 +92,56 @@ class LibsodiumConan(ConanFile):
         msbuild = MSBuild(self)
         msbuild.build(sln_path, upgrade_project=False, platforms={"x86": "Win32"}, build_type=self._vs_configuration)
 
-    def _build_autotools_impl(self, configure_args, host_arch=None):
-        autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
+    def _build_emscripten(self):
+        self.run("./dist-build/emscripten.sh --standard", cwd=self._source_subfolder, win_bash=tools.os_info.is_windows)
+
+    def _configure_autotools(self):
+        if self._autotools:
+            return self._autotools
+
+        yes_no = lambda v: "yes" if v else "no"
+        args = [
+            "--enable-shared={}".format(yes_no(self.options.shared)),
+            "--enable-static={}".format(yes_no(not self.options.shared)),
+            "--enable-soname-versions={}".format(yes_no(self.options.use_soname)),
+            "--enable-pie={}".format(yes_no(not self.options.PIE)),
+        ]
+        if self.options.get_safe("fPIC"):
+            args.append("--with-pic")
+
+        if self.settings.os == "Android":
+            host_arch = "{}-linux-{}".format(tools.to_android_abi(self.settings.arch), self._android_id_str)
+        elif tools.is_apple_os(self.settings.os):
+            host_arch = "{}-apple-{}".format(self.settings.arch, "ios" if self.settings.os == "iOS" else "darwin")
+        elif self._is_mingw:
+            host_arch = "{}-w64-mingw32".format("i686" if self.settings.arch == "x86" else self.settings.arch)
+        elif self.settings.os == "Neutrino":
+            neutrino_archs = {
+                "x86_64": "x86_64-pc",
+                "x86": "i586-pc",
+                "armv7": "arm-unknown",
+                "armv8": "aarch64-unknown",
+            }
+            if self.settings.os.version == "7.0" and str(self.settings.arch) in neutrino_archs:
+                host_arch = "{}-nto-qnx7.0.0".format(neutrino_archs[str(self.settings.arch)])
+                if self.settings.arch == "armv7":
+                    host_arch += "eabi"
+
         host = None
         if host_arch:
             host = False
-            configure_args.append("--host=%s" % host_arch)
-        autotools.configure(args=configure_args, configure_dir=self._source_subfolder, host=host)
-        autotools.make(args=["-j%s" % str(tools.cpu_count())])
-        autotools.install()
+            args.append("--host=%s" % host_arch)
 
-    def _build_autotools_linux(self, configure_args):
-        self._build_autotools_impl(configure_args)
-
-    def _build_autotools_emscripten(self, configure_args):
-        self.run("./dist-build/emscripten.sh --standard", cwd=self._source_subfolder)
-
-    def _build_autotools_android(self, configure_args):
-        host_arch = "%s-linux-%s" % (tools.to_android_abi(self.settings.arch), self._android_id_str)
-        self._build_autotools_impl(configure_args, host_arch)
-
-    def _build_autotools_mingw(self, configure_args):
-        with tools.chdir(self._source_subfolder):
-            self.run("{} -fiv".format(tools.get_env("AUTORECONF")), win_bash=tools.os_info.is_windows)
-        arch = "i686" if self.settings.arch == "x86" else self.settings.arch
-        host_arch = "%s-w64-mingw32" % arch
-        self._build_autotools_impl(configure_args, host_arch)
-
-    def _build_autotools_darwin(self, configure_args):
-        os = "ios" if self.settings.os == "iOS" else "darwin"
-        host_arch = "%s-apple-%s" % (self.settings.arch, os)
-        self._build_autotools_impl(configure_args, host_arch)
-
-    def _build_autotools_neutrino(self, configure_args):
-        neutrino_archs = {"x86_64":"x86_64-pc", "x86":"i586-pc", "armv7":"arm-unknown", "armv8": "aarch64-unknown"}
-        if self.settings.os.version == "7.0" and str(self.settings.arch) in neutrino_archs:
-            host_arch = "%s-nto-qnx7.0.0" % neutrino_archs[str(self.settings.arch)]
-            if self.settings.arch == "armv7":
-                host_arch += "eabi"
-        else:
-            raise ConanInvalidConfiguration("Unsupported arch or Neutrino version for libsodium: {} {}".format(self.settings.os, self.settings.arch))
-        self._build_autotools_impl(configure_args, host_arch)
+        self._autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
+        self._autotools.configure(args=args, configure_dir=self._source_subfolder, host=host)
+        return self._autotools
 
     def _build_autotools(self):
-        absolute_install_dir = os.path.abspath(os.path.join(".", "install"))
-        absolute_install_dir = absolute_install_dir.replace("\\", "/")
-        configure_args = self._get_configure_args(absolute_install_dir)
-
-        if self.settings.os in ["Linux", "FreeBSD"]:
-            self._build_autotools_linux(configure_args)
-        elif self.settings.os == "Emscripten":
-            self._build_autotools_emscripten(configure_args)
-        elif self.settings.os == "Android":
-            self._build_autotools_android(configure_args)
-        elif tools.is_apple_os(self.settings.os):
-            self._build_autotools_darwin(configure_args)
-        elif self._is_mingw:
-            self._build_autotools_mingw(configure_args)
-        elif self.settings.os == "Neutrino":
-            self._build_autotools_neutrino(configure_args)
-        else:
-            raise ConanInvalidConfiguration("Unsupported os for libsodium: {}".format(self.settings.os))
+        if self._is_mingw:
+            with tools.chdir(self._source_subfolder):
+                self.run("{} -fiv".format(tools.get_env("AUTORECONF")), win_bash=tools.os_info.is_windows)
+        autotools = self._configure_autotools()
+        autotools.make()
 
     def validate(self):
         if self.settings.compiler == "Visual Studio":
@@ -162,15 +153,39 @@ class LibsodiumConan(ConanFile):
             tools.patch(**patch)
         if self.settings.os == "Macos":
             tools.replace_in_file(os.path.join(self._source_subfolder, "configure"), r"-install_name \$rpath/", "-install_name ")
-        if self.settings.compiler != "Visual Studio":
-            self._build_autotools()
-        else:
+        if self.settings.compiler == "Visual Studio":
             self._build_visual()
+        elif self.settings.os == "Emscripten":
+            self._build_emscripten()
+        else:
+            self._build_autotools()
+
+    def _package_visual(self):
+        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        inc_src = os.path.join(self._source_subfolder, "src", self.name, "include")
+        self.copy("*.h", src=inc_src, dst="include", keep_path=True, excludes=("*/private/*"))
+
+    def _package_emscripten(self):
+        prefix = os.path.join(self._source_subfolder, "libsodium-js")
+        lib_folder = os.path.join(prefix, "lib")
+        self.copy("*.h", dst="include", src=os.path.join(prefix, "include"))
+        self.copy("*.a", dst="lib", src=lib_folder)
+        self.copy("*.so*", dst="lib", src=lib_folder, symlinks=True)
+        self.copy("*.dylib", dst="lib", src=lib_folder, symlinks=True)
+
+    def _package_autotools(self):
+        autotools = self._configure_autotools()
+        autotools.install()
+        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
+        tools.remove_files_by_mask(os.path.join(self.package_folder, "lib"), "*.la")
 
     def package(self):
         self.copy("*LICENSE", dst="licenses", keep_path=False)
         if self.settings.compiler == "Visual Studio":
             self._package_visual()
+        elif self.settings.os == "Emscripten":
+            self._package_emscripten()
         else:
             self._package_autotools()
 
@@ -181,36 +196,3 @@ class LibsodiumConan(ConanFile):
         self.cpp_info.libs = tools.collect_libs(self)
         if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.system_libs = ["pthread"]
-
-    def _package_autotools(self):
-        if self.settings.os == "Emscripten":
-            prefix = "%s/libsodium-js" % self._source_subfolder
-        else:
-            prefix = "install"
-        lib_folder = os.path.join(prefix, "lib")
-        self.copy("*.h", dst="include", src=os.path.join(prefix, "include"))
-        self.copy("*.a", dst="lib", src=lib_folder)
-        self.copy("*.so*", dst="lib", src=lib_folder, symlinks=True)
-        self.copy("*.dylib", dst="lib", src=lib_folder, symlinks=True)
-
-    def _package_visual(self):
-        self.copy("*.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        inc_src = os.path.join(self._source_subfolder, "src", self.name, "include")
-        self.copy("*.h", src=inc_src, dst="include", keep_path=True, excludes=("*/private/*"))
-
-    def _autotools_bool_arg(self, arg_base_name, value):
-        prefix = "--enable-" if value else "--disable-"
-        return prefix + arg_base_name
-
-    def _get_configure_args(self, absolute_install_dir):
-        args = [
-            "--prefix=%s" % absolute_install_dir,
-            self._autotools_bool_arg("shared", self.options.shared),
-            self._autotools_bool_arg("static", not self.options.shared),
-            self._autotools_bool_arg("soname-versions", self.options.use_soname),
-            self._autotools_bool_arg("pie", self.options.PIE)
-        ]
-        if self.options.get_safe("fPIC"):
-            args.append("--with-pic")
-        return args
